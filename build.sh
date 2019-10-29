99c1e8fbdce70498ec895dfb474f49e274a67734
Eliminater74 <eliminater74@gmail.com>
2019-10-29 17:41:41 -0400

    build script: Add some comments:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+# git am ../android-busybox-ndk/patches/
+# patch -p1 < ../android-busybox-ndk/patches/
+
+
 export PATH="/home/eliminater74/Android/kitchen/android-toolchain2/bin:$PATH"
 export CROSS_COMPILE="/home/eliminater74/Android/kitchen/android-toolchain2/bin/arm-linux-androideabi-"
 make ARCH=arm CROSS_COMPILE="$CROSS_COMPILE"
