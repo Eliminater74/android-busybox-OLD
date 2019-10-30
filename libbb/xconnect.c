2e618cd9a6b95f385870c88db335934b14d73042
Eliminater74 <eliminater74@gmail.com>
2019-10-29 20:29:50 -0400

    libbb: [FIX] Hack fix that was supposed to fix static compiling:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- libbb/xconnect.c
+++ libbb/xconnect.c
@@ -11,11 +11,11 @@
 #include <netinet/in.h>
 #include <net/if.h>
 #include <sys/un.h>
-#include <netdb.h>
-#include "../libres/dietdns.h"
 #if ENABLE_IFPLUGD || ENABLE_FEATURE_MDEV_DAEMON || ENABLE_UEVENT
 # include <linux/netlink.h>
 #endif
+//#include <netdb.h>
+//#include "../libres/dietdns.h"
 #include "libbb.h"
 
 int FAST_FUNC setsockopt_int(int fd, int level, int optname, int optval)
@@ -287,7 +287,7 @@ IF_NOT_FEATURE_IPV6(sa_family_t af = AF_INET;)
 	 * for each possible socket type (tcp,udp,raw...): */
 	hint.ai_socktype = SOCK_STREAM;
 	hint.ai_flags = ai_flags & ~DIE_ON_ERROR;
-	rc = diet_getaddrinfo(host, NULL, &hint, &result); /* hack to use dietlibc's resolver to work around static compile issue */
+	rc = getaddrinfo(host, NULL, &hint, &result); /* hack to use dietlibc's resolver to work around static compile issue */
 	if (rc || !result) {
 		bb_error_msg("bad address '%s'", org_host);
 		if (ai_flags & DIE_ON_ERROR)
@@ -314,7 +314,7 @@ IF_NOT_FEATURE_IPV6(sa_family_t af = AF_INET;)
 	set_nport(&r->u.sa, htons(port));
  ret:
 	if (result)
-		diet_freeaddrinfo(result); /* hack to use dietlibc's resolver to work around static compile issue */
+		freeaddrinfo(result); /* hack to use dietlibc's resolver to work around static compile issue */
 	return r;
 }
 #if !ENABLE_FEATURE_IPV6
